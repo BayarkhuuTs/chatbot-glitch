d4b5ea71a3fe799a2d9004352a619a1ecbc2b30b
Glitch (messenger-platform-quick-start) <none>
2021-06-14 10:40:11 +0000

    👮📷 Checkpoint ./app.js:32829727/844

--- app.js
+++ app.js
@@ -49,8 +49,10 @@ app.post("/webhook", (req, res) => {
 // Accepts GET requests at the /webhook endpoint
 app.get("/webhook", (req, res) => {
   /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>";
-
+  const VERIFY_TOKEN = "EAANWLkIOvSsBANB08Eo81QWIgpEdAgsGOly5pNdAsSPJGTJyQqPw749j2HqKMDaE5x6NTHriRuShwfHwI3HuP6j28HdheobIpW1MPZCDIg35slH6CcgvU0f5Rgi2bG90LRZBLlT94hYxfMqnZCK9uDCGHbitSDzpDAV4sKXwSAdudk4oLoQ";
+  const FACEBOOK_PAGE_ID = 105645051758162
+  const FACEBOOK_APP_ID = 939181606878507
+  const FACEBOOK_APP_SECRET = '0a5b2b43e04bea1dab7f8a1ce0e81494'
   // Parse params from the webhook verification request
   let mode = req.query["hub.mode"];
   let token = req.query["hub.verify_token"];
